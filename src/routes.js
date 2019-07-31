2bf1f36eca090b7e8c6ad9b12a3d15c62f3dea3e
ok <notabeech@gmail.com>
2019-08-01 04:55:50 +0500

    video_article related videos

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 
 import Home from './components/Home/home';
 import NewsArticle from './components/Articles/News/Post/index';
+import VideoArticle from './components/Articles/Videos/Video/index';
 import Layout from './hoc/Layout/layout';
 
 export default class routes extends Component {
@@ -12,6 +13,7 @@ export default class routes extends Component {
         <Switch>
           <Route path="/" exact component={Home}/>
           <Route path="/articles/:id" exact component={NewsArticle}/>
+          <Route path="/videos/:id" exact component={VideoArticle}/>
         </Switch>
       </Layout>
     )
